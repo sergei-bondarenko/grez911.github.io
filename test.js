133c4c40d39d17fd77c951c57b3f8c7f71c71ee3
grez <grez@PC>
2015-11-29 16:47:27 +0300

    set drawbox size

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var test = function () {
-  var pagecontent = "<div id=\"drawbox\"><svg id=\"drawsvg\"></svg></div>";
+  var pagecontent = "<div id=\"drawbox\" height=500 width=500><svg id=\"drawsvg\"></svg></div>";
   $("body").append(pagecontent);
   s = Snap("#drawsvg");                     // создаём холст
   s.attr({viewBox: "0 0 500 500", width: "100%", height: "100%"});
